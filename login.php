e5343bab26aeb1fc5672b62a1a36ee2ebe9e5ca5
scrapdigital <31674037+scrapdigital@users.noreply.github.com>
2017-09-05 19:46:59 -0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,70 @@
+<!Doctype html>
+<html lang="pt-br">
+	<head>
+		<title>Scrap</title>
+		<meta charset="utf-8">
+
+		<!--<link href="style.css" rel="stylesheet" type="text/css"> -->
+		<!--Import Google Icon Font-->
+      	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+		<!-- Materialize CSS -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+		<link rel="icon" href = "tt.png">
+	</head>
+	<body>
+		<header>
+				<nav>
+					<div class="nav-wrapper grey darken-3">
+					 <!--<a href="custom/logo.jpg" class="brand-logo">Gotham Center</a>-->
+					  <!--<a href="index.html" class="brand-logo center"><i class="material-icons">event_seat</i>Scrap</a>-->
+
+      <a href="index.html" class="brand-logo center"><i class="material-icons">event_seat</i>Scrap</a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="trabalheconosco.php" class="waves-effect grey darken-2 btn">Trabalhe Conosco <i class="material-icons right">computer</i></a></li>
+        <li><a href="login.php" class="waves-effect grey darken-2 btn">Login<i class="material-icons right">cloud</i></a></li>
+      </ul>
+		</div>
+		</nav>
+		</header>
+		<div class="col s m5">
+		<div class="card-panel center grey lighten-2">
+
+
+		<div class="row">
+      <div class="col s4 offset-s4">
+        <div class="card-panel grey darken-3">
+          <span class="white-text">Login</span>
+        </div>
+      </div>
+    </div>
+
+    
+ 		
+			<div class="row">
+        	<div class="input-field offset-s4 col s4">
+          		<input id="email" type="email" class="validate">
+          		<label class="active" for="email">Email</label>
+        	</div>
+        	</div>
+
+        	<div class="row">
+       		 <div class="input-field offset-s4 col s4">
+         		 <input id="password" type="password" class="validate">
+          		<label class="active" for="password">Senha</label>
+       		 </div>
+     	 </div>
+     	 <button class="btn waves-effect grey darken-3 waves-light" type="submit" name="action">Entrar
+    		<i class="material-icons right">send</i>
+  		</button>
+  		<a href="trabalheconosco.php"><button class="btn waves-effect grey darken-3 waves-light" type="submit" name="action">Inscreva-se
+    		<i class="material-icons right">info</i>
+  		</button></a>
+
+  		<br>
+  		<br>
+		</div>
+
+		</div>
+		</body>
+		</html>
---
+++ trabalheconosco.php
@@ -0,0 +1,98 @@
+<!Doctype html>
+<html lang="pt-br">
+  <head>
+    <title>Scrap</title>
+    <meta charset="utf-8">
+
+    <!--<link href="style.css" rel="stylesheet" type="text/css"> -->
+    <!--Import Google Icon Font-->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- Materialize CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+    <link rel="icon" href = "tt.png">
+  </head>
+  <body>
+    <header>
+        <nav>
+          <div class="nav-wrapper grey darken-3">
+           <!--<a href="custom/logo.jpg" class="brand-logo">Gotham Center</a>-->
+            <!--<a href="index.html" class="brand-logo center"><i class="material-icons">event_seat</i>Scrap</a>-->
+
+      <a href="index.html" class="brand-logo center"><i class="material-icons">event_seat</i>Scrap</a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="trabalheconosco.php" class="waves-effect grey darken-2 btn">Trabalhe Conosco <i class="material-icons right">computer</i></a></li>
+        <li><a href="login.php" class="waves-effect grey darken-2 btn">Login <i class="material-icons right">cloud</i></a></li>
+      </ul>
+          </div>
+        </nav>
+    </header>
+<div class="card-panel center grey lighten-2">
+
+  <div class="row">
+      <div class="col s12 m5">
+        <div class=" center card-panel grey darken-3">
+          <span class="white-text">Trabalhe Conosco</span>
+        </div>
+      </div>
+    </div>
+
+    <div id="first-temp" class="row">
+        <div class="col s12 m6 l12">
+          <div class="card blue-grey darken-1">
+            <div class="card-content white-text">
+              <span class="card-title">Seja um de nossos técnicos</span>
+             <p>Trabelhe conosco de forma eficiente e rápida, sem compromisso de horarios e ganhos mensais de até R$1000,00 extra, trabelhe a hora que quiser atendendo nossos chamados em casa pagando apenas uma taxa mensal de 20% para o Scrap de acordo com os seus ganhos, por exemplo: Digamos que você ganhou R$1000,00 em um mês, você terá que pagar apenas R$200,00 para nós e terá 80% de lucros com seus ganhos.
+              Você pode escolher trabalhar em dois modos diferentes, pode escolher trabalhar com suporte pessoal que é feito em casa para pessoas fisicas, com o suporte empresarial que é feito em empresas e pessoas juridicas, os suportes serão feitos de forma segura e eficiênte, os valores de cada chamado serão especificados em cada um deles.</p>
+            </div>
+            <div class="card-action">
+              <a href="#form">Inscreva-se e comece em 24 horas</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    <div class="card teal lighten-5">
+    <div class="row">
+    <form id="form" class="col s12">
+    <h4>Inscrição</h4>
+    <br>
+      <div class="row">
+        <div class="input-field col s6">
+          <input id="first_name" type="text" class="validate">
+          <label class="active" for="first_name">Nome</label>
+        </div>
+        <div class="input-field col s6">
+          <input id="last_name" type="text" class="validate">
+          <label class="active" for="last_name">Sobrenome</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s6">
+          <input id="password" type="password" class="validate">
+          <label class="active" for="password">Crie sua senha</label>
+        </div>
+        <div class="input-field col s6">
+          <input id="email" type="email" class="validate">
+          <label class="active" for="email">Email</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <textarea id="textarea1" class="materialize-textarea"></textarea>
+          <label class="active" for="textarea1">Digite Suas Experiencias Na Area</label>
+        </div>
+      </div>
+      <button class="btn waves-effect grey darken-3 waves-light left" type="submit" name="action">Submit
+    <i class="material-icons right">send</i>
+  </button>
+  <br>
+  <!--<span>Ao clicar em enviar você está aceitando os termos</span> -->
+      <br>
+    </form>
+  </div>
+      </div>
+
+    </div>
+    </body>
+    </html>
